15434b6b0b5fb65a8239744480a6d51929c6ec43
LOANSI SAS <hecfapugo@gmail.com>
2025-05-20 07:47:07 -0500

    Update decorators.py

--- app/decorators.py
+++ app/decorators.py
@@ -11,10 +11,20 @@ def admin_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-def vendedor_required(f):
+def vendedor_extended_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not current_user.is_authenticated or not (current_user.is_vendedor() or current_user.is_admin()):
+            flash('No tienes permisos para acceder a esta página.', 'danger')
+            return redirect(url_for('dashboard.index'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+# Decorador que permite acceso a vendedores y cobradores
+def vendedor_cobrador_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_authenticated or not (current_user.is_vendedor() or current_user.is_cobrador() or current_user.is_admin()):
             flash('No tienes permisos para acceder a esta página.', 'danger')
             return redirect(url_for('dashboard.index'))
         return f(*args, **kwargs)
