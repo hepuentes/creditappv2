c4f4b5bc9f2cec572e1e8e3bd15795353ed445dd
LOANSI SAS <hecfapugo@gmail.com>
2025-06-05 14:02:58 -0500

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -6,4 +6,6 @@ api = Blueprint('api', __name__, url_prefix='/api/v1')
 # Importar todos los módulos de la API
 from app.api import auth
 from app.api import sync_data
-from app.api import clientes  
+from app.api import clientes
+from app.api import ventas
+from app.api import abonos
